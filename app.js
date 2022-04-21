67d2de81aabae0388df5da74835c724a109859ca
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-21 15:44:07 -0500

    Created route /

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ const express = require('express');
 const app = express();
 port = 3000;
 
+// Indicamos el initial path, para la url `localhost: 3000`
+app.get('/', (req, res) => {
+	res.send('Hello Server');
+});
 
+// Inicializamos la app
+app.listen(port, () => {
+	console.log(`This server is listening in port ${port}`);
+});
