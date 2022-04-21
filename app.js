4c84e1de25bbc9e7b7428d6090f09ca096771564
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-04-21 16:08:50 -0500

    Added route /explorers that receives a param object

--- app.js
+++ app.js
@@ -10,6 +10,27 @@ app.get('/', (req, res) => {
 	res.send('Hello Server');
 });
 
+// Agregando una nueva ruta localhost:3000/launchx
+// Y respondiendo con text
+app.get('/launchx', (req, res) => {
+	res.send('Welcome to Launch X');
+});
+
+// Retornando un objecto al ingresar a una ruta
+app.get('/explorers', (req, res) => {
+	const explorer = { name: 'Fabian', msg: 'Hi'}
+	res.send(explorer);
+});
+
+// Agregando una ruta que recibe parametros
+// :name indica que sera un valor enviado por la url
+// Entonces se recibe un objeto params que contiene la variable y su valor
+// req.params = {'name': 'Fabian'}
+app.get('/explorers/:name', (req, res) => {
+	res.send(req.params);
+})
+
+
 // Inicializamos la app
 app.listen(port, () => {
 	console.log(`This server is listening in port ${port}`);
